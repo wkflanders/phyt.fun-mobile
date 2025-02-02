474dda0db85baf4d25ad4dd638ad61d97259486a
wkflanders <wflan@seas.upenn.edu>
2025-02-02 00:11:37 -0500

    onboard page

---
+++ app/onboard.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function Onboard() {
+    return (
+        <View>
+            <Text>Onboard</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
