3c9bdbea4d9aad8386a81d4a526d99bd651a6718
wkflanders <wflan@seas.upenn.edu>
2025-02-01 22:09:02 -0500

    root layout setup + privy

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { PrivyProvider } from '@privy-io/expo';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from '@expo-google-fonts/inter';
+import { baseSepolia } from 'viem/chains';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -13,27 +14,50 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded, error] = useFonts({
+    "Inconsolata-ExtraLight": require("../assets/fonts/Inconsolata-ExtraLight.ttf"),
+    "Inconsolata-Light": require("../assets/fonts/Inconsolata-Light.ttf"),
+    "Inconsolata-Regular": require("../assets/fonts/Inconsolata-Regular.ttf"),
+    "Inconsolata-Medium": require("../assets/fonts/Inconsolata-Medium.ttf"),
+    "Inconsolata-Bold": require("../assets/fonts/Inconsolata-Bold.ttf"),
+    "Inconsolata-SemiBold": require("../assets/fonts/Inconsolata-SemiBold.ttf"),
+    "Inconsolata-ExtraBold": require("../assets/fonts/Inconsolata-ExtraBold.ttf"),
+    "Inconsolata-Black": require("../assets/fonts/Inconsolata-Black.ttf"),
+    "Inter-ExtraLight": require("../assets/fonts/Inter-ExtraLight.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+    "Inter-Thin": require("../assets/fonts/Inter-Thin.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-ExtraBold": require("../assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
   });
 
   useEffect(() => {
-    if (loaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [fontsLoaded]);
 
-  if (!loaded) {
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <PrivyProvider
+      appId={'cm466mv4o01wfkhkse3g9gyhr'}
+      clientId={'client-WY5eJqKxgS2bURn6XZU2CTYFMphvJ8X9he8fipPukPvKH'}
+      supportedChains={[baseSepolia]}
+    >
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="onboarding" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </PrivyProvider >
   );
 }
