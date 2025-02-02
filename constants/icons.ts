ebbe037a573847644d75a8b4755c8bb512f30757
wkflanders <wflan@seas.upenn.edu>
2025-02-02 15:17:01 -0500

    icons export

---
+++ constants/icons.ts
@@ -0,0 +1,46 @@
+const home = require("@/assets/icons/home.png");
+const play = require("@/assets/icons/play.png");
+const friends = require("@/assets/icons/friends.png");
+const leaderboard = require("@/assets/icons/leaderboard.png");
+const rewards = require("@/assets/icons/rewards.png");
+const eye = require("@/assets/icons/eye.png");
+const eyehide = require("@/assets/icons/eye-hide.png");
+const run = require("@/assets/icons/run.png");
+const settings = require("@/assets/icons/settings.png");
+const wallet = require("@/assets/icons/wallet.png");
+const profile = require("@/assets/icons/profile.png");
+const message = require("@/assets/icons/message.png");
+const start = require("@/assets/icons/start.png");
+const stop = require("@/assets/icons/stop.png");
+const like = require("@/assets/icons/like.png");
+const heart = require("@/assets/icons/heart.png");
+const celebrate = require("@/assets/icons/celebrate.png");
+const comment = require("@/assets/icons/comment.png");
+const share = require("@/assets/icons/share.png");
+const send = require("@/assets/icons/send.png");
+const close = require("@/assets/icons/close.png");
+
+
+export default {
+    home,
+    play,
+    friends,
+    leaderboard,
+    rewards,
+    eye,
+    eyehide,
+    run,
+    settings,
+    wallet,
+    profile,
+    message,
+    start,
+    stop,
+    like,
+    heart,
+    celebrate,
+    comment,
+    share,
+    send,
+    close
+};
