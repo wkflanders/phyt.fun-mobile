d15cb33ade8dc423fa24bd9fc04575f3b7e12788
wkflanders <wflan@seas.upenn.edu>
2025-02-02 03:34:02 -0500

    images export

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+const onboardLogo = require('../assets/images/onboard-logo.png');
+const P_logo = require('../assets/images/P_logo.png');
+const logo = require('../assets/images/logo.png');
+
+export default {
+    onboardLogo,
+    P_logo,
+    logo
+};
